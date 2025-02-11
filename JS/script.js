aab0d15834b8f271f3a64c6f99543c1bb6f8ba74
techtogreen02@gmail.com <techtogreen02@gmail.com>
2025-02-12 01:43:48 +0530

    fixed severed links

--- JS/script.js
+++ JS/script.js
@@ -34,16 +34,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-/*-----------------Loading JS-----------------*/
-
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(() => {
-      document.querySelector(".preloader").style.opacity = "0"; // Fade out
-      setTimeout(() => {
-          document.querySelector(".preloader").style.display = "none"; // Remove
-      }, 500); // Small delay for smooth transition
-  }, 4000); // 4s delay
-});
 
 // Footer JS --------------------------------
 
